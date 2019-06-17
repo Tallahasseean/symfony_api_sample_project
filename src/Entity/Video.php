715b8268a9f341e128ae2d1f23f492a2e748b4d4
Casey Smith <caseyy@gmail.com>
2019-06-16 21:27:02 -0400

    Add Entities, Repositories, and migrations for videos and events

---
+++ src/Entity/Video.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VideoRepository")
+ */
+class Video
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $thumbnail;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $playlistUrl;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getThumbnail(): ?string
+    {
+        return $this->thumbnail;
+    }
+
+    public function setThumbnail(?string $thumbnail): self
+    {
+        $this->thumbnail = $thumbnail;
+
+        return $this;
+    }
+
+    public function getPlaylistUrl(): ?string
+    {
+        return $this->playlistUrl;
+    }
+
+    public function setPlaylistUrl(?string $playlistUrl): self
+    {
+        $this->playlistUrl = $playlistUrl;
+
+        return $this;
+    }
+}
